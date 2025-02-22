c821c016e2d0e7636c1e0bda341fecfb46f565b5
yurkenchik <yura.ilchshyn@gmail.com>
2025-02-22 11:29:50 +0200

    added validation for create order dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,6 +1,11 @@
+import {IsNotEmpty, IsUUID} from "class-validator";
 
 export class CreateOrderDto {
+    @IsUUID()
+    @IsNotEmpty()
     readonly userId: string;
+    @IsUUID()
+    @IsNotEmpty()
     readonly productId: string;
     readonly quantity: number;
 }
